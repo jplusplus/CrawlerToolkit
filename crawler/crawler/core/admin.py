2f0f80e5d1ce02e62277e39f03dd4cd70a865877
pbellon <bellon.pierre@gmail.com>
2017-10-31 20:43:33 +0100

    Add link to edit profile from admin index

--- crawler/crawler/core/admin.py
+++ crawler/crawler/core/admin.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from material.frontend.models import Module
 from crawler.core.models import Feed, Article
 
 def force_crawl_feeds(modeladmin, request, queryset):
     from crawler.core.tasks_utils import crawl_feeds
     crawl_feeds(queryset)
+
+def _icon(icon_name): return '<i class="material-icons">%s</i>' % icon_name
 
 @admin.register(Feed)
 class FeedAdmin(admin.ModelAdmin):
@@ -13,11 +17,15 @@ class FeedAdmin(admin.ModelAdmin):
     fields = ('active', 'url', 'name')
     list_display = ('name', 'url', 'last_time_crawled')
     readomly_fields = ('last_time_crawled',)
-    icon = '<i class="material-icons">rss_feed</i>'
+    icon = _icon('rss_feed')
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('source','url', 'created_at', 'should_preserve')
+    icon = _icon('description')
 
 
+# Unregister unused models.
+admin.site.unregister(Group)
+admin.site.unregister(Module)
