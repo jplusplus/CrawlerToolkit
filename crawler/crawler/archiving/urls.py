f24fde8e21340f90399557a514a6a475413aba1b
pbellon <bellon.pierre@gmail.com>
2017-11-05 19:33:52 +0100

    Improve preview url

--- crawler/crawler/archiving/urls.py
+++ crawler/crawler/archiving/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from crawler.archiving import views
 
 urlpatterns = [
-    url(r'^(?P<feed_slug>[\w-]+)/(?P<article_slug>[\w-]+)/',
+    url(r'^(?P<feed_slug>[\w-]+)/(?P<article_slug>[\w-]+)/$',
         views.serve_article,
         name='serve_article'),
-    url(r'^(?P<feed_slug>[\w-]+)/(?P<article_slug>[\w-]+)/preview',
+    url(r'^(?P<feed_slug>[\w-]+)/(?P<article_slug>[\w-]+)/preview/$',
         views.preview_article,
         name='preview_article'),
 ]
