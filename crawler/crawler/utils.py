cde527980c61e8163f1795c2634a5954e2a05b5a
pbellon <bellon.pierre@gmail.com>
2017-11-17 17:25:11 +0100

    Add debug count methods into utils.py

--- crawler/crawler/utils.py
+++ crawler/crawler/utils.py
@@ -1,5 +1,7 @@
+from django.db import connection
 from django.conf import settings
 from urllib.parse import urlparse
+from pprint import pprint
 
 def pickattr(qs, attrname):
     return list(map(lambda obj:getattr(obj, attrname), qs))
@@ -25,3 +27,27 @@ def mediaurl(path):
         url = "{url}?{query}".format(url=url, query=parsed_path.query)
 
     return url
+
+
+
+def modelcounts(sort=False, reverse=True):
+    count = lambda model: (
+        model, model.objects.count() if getattr(model, 'objects', None) else 0
+    )
+    tables = connection.introspection.table_names()
+    modelclasses = connection.introspection.installed_models(tables)
+    counts =  map(count, modelclasses)
+    if sort:
+        order = -1 if reverse else 1
+        counts = sorted(counts, key=lambda e: order * e[1])
+
+    return counts
+
+
+def print_counts(**kwargs):
+    todict = lambda c: {
+        "%s.%s" % (c[0]._meta.app_label, c[0]._meta.verbose_name): c[1]
+    }
+    pprint(
+        list(map(todict, modelcounts(**kwargs)))
+    )
