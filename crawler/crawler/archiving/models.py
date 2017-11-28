e8b54641eefde1e614f16a58659380f0326749e5
pbellon <bellon.pierre@gmail.com>
2017-11-28 16:41:56 +0100

    Augment max_length for URL to avoid DB error

--- crawler/crawler/archiving/models.py
+++ crawler/crawler/archiving/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class ArchivedArticle(models.Model):
     service = models.CharField(max_length=100, null=False, blank=False)
     article = models.ForeignKey('core.Article', related_name='archived_urls')
-    url = models.URLField(blank=False,null=False)
+    url = models.URLField(max_length=450, blank=False,null=False)
