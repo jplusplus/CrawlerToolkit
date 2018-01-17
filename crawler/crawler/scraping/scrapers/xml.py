c4c8117e177f245f64f830c4aa84ae355e65c66a
pbellon <bellon.pierre@gmail.com>
2018-01-17 20:17:01 +0100

    Add tests for scraping module (XML feeds)

--- crawler/crawler/scraping/scrapers/xml.py
+++ crawler/crawler/scraping/scrapers/xml.py
@@ -9,7 +9,7 @@ import requests
 def scrape(url):
     req = requests.get(url)
     xml = etree.fromstring(req.text.encode('utf-8'))
-    _ns = xml.nsmap[None]
+    _ns = xml.nsmap.get(None, '')
     if _ns and _ns.endswith('Atom'):
         # We have to setup a namespace to properly query the xml with xpath
         ns = 'http://www.w3.org/2005/Atom'
--- crawler/crawler/scraping/tests.py
+++ crawler/crawler/scraping/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from crawler.scraping.scrapers import page_metas
+from crawler.scraping.scrapers import page_metas, xml as xmlscraper
 
 import requests_mock
 
@@ -34,3 +34,59 @@ class PageMetasTestCase(TestCase):
         self.checkMetas(
             page_metas.parse(self.fake_html)
         )
+
+class XMLFeedTestCase(TestCase):
+    def setUp(self):
+        self.feed_url = 'http://fake.com/feed.xml'
+        self.link_a = 'http://fake.com/posts/1'
+        self.link_b = 'http://fake.com/posts/2'
+
+    def check_if_links_are_presents(self, links):
+        return self.link_a in links and self.link_b in links
+
+    def runTestWith(self, m, xml):
+        xml = xml.format(link_a=self.link_a, link_b=self.link_b)
+        m.register_uri('GET', self.feed_url, text=xml)
+        links = xmlscraper.scrape(self.feed_url)
+        self.assertIn(self.link_a, links)
+        self.assertIn(self.link_b, links)
+
+    @requests_mock.Mocker()
+    def test_xml_rss(self, m):
+        self.runTestWith(m, '''<?xml version="1.0" encoding="utf-8"?>
+            <rss version="2.0">
+                <channel>
+                    <title>Fake</title>
+                    <link>https://fake.com</link>
+                    <item>
+                        <link>{link_a}</link>
+                    </item>
+                    <item>
+                        <link>{link_b}</link>
+                    </item>
+                </channel>
+            </rss>
+            '''
+        )
+
+    @requests_mock.Mocker()
+    def test_xml_atom(self, m):
+        self.runTestWith(m, '''<?xml version="1.0" encoding="utf-8"?>
+        <feed xmlns="http://www.w3.org/2005/Atom">
+            <title>Fil d'exemple</title>
+            <subtitle>Un titre secondaire.</subtitle>
+            <link href="http://example.org/"/>
+            <updated>2010-05-13T18:30:02Z</updated>
+            <author>
+                 <name>Paul Martin</name>
+                 <email>paulmartin@example.com</email>
+             </author>
+             <id>urn:uuid:60a76c80-d399-11d9-b91C-0003939e0af6</id>
+             <entry>
+               <link href="{link_a}"/>
+             </entry>
+             <entry>
+               <link href="{link_b}"/>
+             </entry>
+        </feed>
+        ''')
