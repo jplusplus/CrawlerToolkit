afd5466551c41861d03299e91e8dac89ecab49ea
pbellon <bellon.pierre@gmail.com>
2018-01-22 16:05:56 +0100

    Put back archive_article into archive.tasks (instead of archiving.__init__)

--- crawler/crawler/archiving/tasks.py
+++ crawler/crawler/archiving/tasks.py
@@ -1,11 +1,54 @@
 from django.urls import reverse
+
 from celery.decorators import task
 from celery.utils import log
+
 from crawler import utils
-from crawler.archiving import archive_article, detect_notfound
+from crawler.archiving import services as archive
+from crawler.archiving.scrapers import detect_notfound
 from crawler.archiving.models import ArchivedArticle
 
+from crawler.constants import STATES
+
 logger = log.get_task_logger(__name__)
+
+def archive_article(article):
+    from crawler.archiving.models import ArchivedArticle
+    service_name = ''
+    archived_url = ''
+    # First case: we have no preservation, thus in case of
+    # preservation:priority meta tag.
+    article_url = article.url
+
+    # Second case: every article that has preservation needs thus other meta
+    # tags
+    if article.preservation_state != STATES.PRESERVATION.NO_PRESERVE:
+        article_path = reverse('store:serve_article', kwargs={
+            'feed_slug':article.feed.slug,
+            'article_slug':article.slug
+        })
+        article_url = utils.absurl(article_path)
+
+    try:
+        for service in archive.services(article_url):
+            service_name = service.name()
+            prev_archive = ArchivedArticle.objects.filter(
+                service=service_name,
+                article=article
+            )
+            if not prev_archive.exists():
+                archived_url = service.start()
+                ArchivedArticle.objects.create(
+                    service=service_name,
+                    url=archived_url,
+                    article=article)
+
+        article.archiving_state = STATES.ARCHIVE.ARCHIVED
+        article.save()
+    except Exception as e:
+        article.archiving_state = STATES.ARCHIVE.ERROR
+        article.save()
+        raise e
 
 @task(ignore_results=True)
 def archive_articles(ids=None, skip_filter=False):
